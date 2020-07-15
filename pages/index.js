d244bafa0f6056c576833ce20518e248f67fc139
Akhil Mudgal <akhil.mud@gmail.com>
2020-07-15 16:27:15 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -36,16 +36,6 @@ const Home = () => (
           <h3>Contact Details &rarr;</h3>
           <p>094138 41697</p>
         </a>
-
-        <a
-          href="https://vercel.com/new?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>
-            Instantly deploy your Next.js site to a public URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
 
